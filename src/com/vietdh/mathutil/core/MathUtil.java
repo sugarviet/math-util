a7c6a8d7a5d78aace74c4851f8724ac45f8a71f0
Viet Dang <vietdanghoang1705@gmail.com>
2022-10-15 09:04:47 +0700

    update getFactorial to use recursion

--- src/com/vietdh/mathutil/core/MathUtil.java
+++ src/com/vietdh/mathutil/core/MathUtil.java
@@ -9,17 +9,27 @@ package com.vietdh.mathutil.core;
  * @author Dang Viet
  */
 public class MathUtil {
-     public static long getFactorial(int n){
+//     public static long getFactorial(int n){
+//        if(n < 0 || n > 20){
+//            throw new IllegalArgumentException("Invalid argument. N must be between 0 and 20");
+//        }
+//        if(n == 0 && n == 1){
+//            return 1;
+//        }
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    public static long getFactorial(int n){
         if(n < 0 || n > 20){
             throw new IllegalArgumentException("Invalid argument. N must be between 0 and 20");
         }
-        if(n == 0 && n == 1){
-            return 1;
-        }
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+//        if(n == 0 && n == 1){
+//            return 1;
+//        }
+        
+        return n * getFactorial(n - 1);
     }
 }
